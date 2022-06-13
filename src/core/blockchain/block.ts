d8f7d11447e60a9a8fe0be8e2a2d9b2a7d9bff36
lsj <loerain111@gmail.com>
2022-06-13 09:13:00 +0900

    주석 제거

--- src/core/blockchain/block.ts
+++ src/core/blockchain/block.ts
@@ -11,14 +11,10 @@ export class Block extends BlockHeader implements IBlock {
     public nonce : number
 
     constructor(_previousBlock: Block, _data : string[]) {
-        // this : { }
         super(_previousBlock)
-        // this : { previouBlock 속성값을 상속받아 채움}
         const merkleRoot = Block.getMerkleRoot(_data)
         this.merkleRoot = merkleRoot
-        // 여기까지 채운 this를 getMerkleRoot의 인자값으로 준다.
         this.hash = Block.createBlockHash(this)
-        // = 지금까지 만든 이 객체를 넘겨준다
         this.nonce = 0
         this.difficulty = 1
         this.data = _data
@@ -34,16 +30,6 @@ export class Block extends BlockHeader implements IBlock {
     }
 
     public static createBlockHash(_block:Block):string {
-        // { data, hash, ...rest} : Block << 이렇게 매개변수에 줘도 된다.
-        // { version, timetstamp, merkleRoot, previoushash, height }
-
-        // let block2:object = {
-        //     version : _block.version,
-        //     timestamp : _block.timestamp,
-        //     height: _block.height,
-        //     previousHash : _block.previousHash,
-        //     merkelRoot : _block.merkleRoot
-        // }
         const { version, timestamp, merkleRoot, previousHash, height} = _block
 
         const values: string = `${version}${timestamp}${merkleRoot}${previousHash}${height}`
@@ -64,10 +50,6 @@ export class Block extends BlockHeader implements IBlock {
     }
 
     public static isValidNewBlock(_newBlock:Block, _previouseBlock:Block): Failable <Block, string> {
-        // Failable : 에러가 없으면 Block, 있으면 string
-        // 1. 이전블럭 height + 1  === new Block의 height ? 
-        // 2. 이전블럭 해시값 === 현재블럭.이전블럭해시 값 ?
-        // 3. _newBlock의 속성들을 가져와 hash 새로 생성 === _newBlock의 hash ? 
         if (_previouseBlock.height + 1 !== _newBlock.height) return { isError : true, error: 'block height is different'}
         if (_previouseBlock.hash !== _newBlock.previousHash) return { isError : true, error : 'hash value doesn match'}
         if ( Block.createBlockHash(_newBlock) !== _newBlock.hash ) return { isError : true, error : 'hash value is wrong'}
@@ -75,7 +57,3 @@ export class Block extends BlockHeader implements IBlock {
         return {isError : false, value: _newBlock}
     }
 }
-
-/*
- hash = version + timestamp + height + previousHash + merkleRoot  
-*/
